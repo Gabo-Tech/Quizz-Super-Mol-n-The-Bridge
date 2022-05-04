6f85301b72b77fb666a1a9b86efb2e40103d3572
gabriel19971029 <clementeaquipresente@yahoo.com>
2022-05-04 16:33:36 +0200

    más estilos y historia

--- main.js
+++ main.js
@@ -1,1 +1,96 @@
 const API = "https://opentdb.com/api.php?amount=10&category=17&difficulty=hard&type=multiple";
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const text = document.getElementById("text");
+
+
+let currentQuestionIndex;
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+    ],
+  },
+  {
+    question: "Is web development frustrating?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "What is 4 * 2?",
+    answers: [
+      { text: "6", correct: false },
+      { text: "8", correct: true },
+      { text: "Yes", correct: false },
+    ],
+  },
+];
+
+function setStatusClass(element, correct) {
+  if (correct) {
+    element.classList.remove("myButton");
+    element.classList.add("correct");
+  } else {
+    element.classList.remove("myButton");
+    element.classList.add("wrong");
+  }
+}
+
+function selectAnswer() {
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (questions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("myButton");
+    if (answer.correct) {
+      button.dataset.correct = true;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+function setNextQuestion() {
+  resetState();
+  showQuestion(questions[currentQuestionIndex]);
+}
+
+function startGame() {
+  startButton.classList.add("hide");
+  text.classList.add("hide");
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
