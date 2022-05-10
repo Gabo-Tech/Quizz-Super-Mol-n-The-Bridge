bb2c8763a834f01e9805f92aee0ac298d4e70352
Rebeca <rebeca.as1994@gmail.com>
2022-05-10 17:06:43 +0200

    Todo limpito

--- README.md
+++
@@ -1,7 +0,0 @@
-# Quizz Súper Molón The Bridge
-Cosas por hacer:
-[ ] añadir la gráfica
-[ ] solucionar los problemas que nos dé la gráfica
-[ ] tamaño imágenes
-
-[ ] readme
--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,18 @@ const nextButton = document.getElementById("next-btn");
 const textCorrect = document.getElementById("text-correct");
 const textWrong = document.getElementById("text-wrong")
 const startButton = document.getElementById("start-btn");
+const audio = new Audio('./stonecutters-song.mp3');
 const ctx = document.getElementById('myChart');
 const ctx2 = document.getElementById('myChart2')
-//const laMierdaDeLaGráfica = getElementById('contenedor');
-const audio = new Audio('./stonecutters-song.mp3');
+
 let partida = 1;
 let questions = [];
 let correctAnswers = 0;
 let currentQuestionIndex;
 let myChart;
 let myChart2;
-let chartOptions
+let chartOptions;
+
 const getApi = async () => {
   const response = await axios.get("../questions.json")
   questions = response.data.results;
@@ -33,21 +34,19 @@ function setStatusClass(element, correct) {
   }
 }
 
-
 function addData(chart, label, data) {
   chart.data.labels.push(label);
   chart.data.datasets.forEach((dataset) => {
     dataset.data.push(data);
   });
-
 }
 
 function showGraph(arrLocalStorageKeys, arrLocalStorageValues) {
-  console.log(arrLocalStorageKeys, arrLocalStorageValues)
   if (myChart || myChart2) {
     myChart.destroy()
     myChart2.destroy()
   }
+
   chartOptions = {
     type: 'bar',
     data: {
@@ -80,7 +79,7 @@ function showGraph(arrLocalStorageKeys, arrLocalStorageValues) {
           {
             ticks: {
               display: true,
-              stepSize: 1,
+              stepSize: 2,
               min: 0,
               max: 10,
             },
@@ -91,22 +90,21 @@ function showGraph(arrLocalStorageKeys, arrLocalStorageValues) {
         display: false
       }
     }
-
   }
 }
-function correcta() {
 
+function correcta() {
   if (questions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
     if (correctAnswers !== 10) {
-      questionContainerElement.classList.add("hide")
-      textCorrect.classList.add("hide")
-      textWrong.classList.remove("hide")
+      questionContainerElement.classList.add("hide");
+      textCorrect.classList.add("hide");
+      textWrong.classList.remove("hide");
       startButton.innerText = "Vuelve a intentarlo";
       startButton.classList.remove("hide");
-      //ctx.classList.remove("hide");
       localStorage.setItem(partida, JSON.stringify(correctAnswers));
+
       let partidas = []
       for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
@@ -115,31 +113,24 @@ function correcta() {
           numeroPartida: key,
           resultadoPartida: valor
         }
-        console.log(resultado)
-        // arrLocalStorageKeys.unshift(key);
-        // arrLocalStorageValues.unshift(valor);
         partidas.push(resultado)
       }
+
       let partidasOrdenadas = partidas.sort((a, b) => a.numeroPartida - b.numeroPartida)
       let arrLocalStorageKeys = partidasOrdenadas.map(partida => partida.numeroPartida)
       let arrLocalStorageValues = partidasOrdenadas.map(partida => partida.resultadoPartida)
-      console.log(arrLocalStorageKeys, arrLocalStorageValues);
       showGraph(arrLocalStorageKeys, arrLocalStorageValues)
-      // const arrLocalStorageKeys = Object.keys({ ...localStorage });
-      // const arrLocalStorageValues = Object.values({ ...localStorage });
-      //console.log(arrLocalStorageKeys,  arrLocalStorageValues);
-
-
       myChart = new Chart("myChart", chartOptions);
       myChart2 = new Chart("myChart2", chartOptions);
+
     } else {
       audio.play();
       questionContainerElement.classList.add("hide")
       textCorrect.classList.remove("hide");
       startButton.innerText = "Vuelve a intentarlo";
       startButton.classList.remove("hide");
-      //ctx.classList.remove("hide");
       localStorage.setItem(partida, JSON.stringify(correctAnswers));
+
       let partidas = []
       for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
@@ -149,51 +140,16 @@ function correcta() {
           resultadoPartida: valor
         }
         partidas.push(resultado)
-
       }
+
       let partidasOrdenadas = partidas.sort((a, b) => a.numeroPartida - b.numeroPartida)
       let arrLocalStorageKeys = partidasOrdenadas.map(partida => partida.numeroPartida)
       let arrLocalStorageValues = partidasOrdenadas.map(partida => partida.resultadoPartida)
-      console.log(arrLocalStorageKeys, arrLocalStorageValues);
       showGraph(arrLocalStorageKeys, arrLocalStorageValues)
-      // new Chart(ctx, {
-      //   type: 'bar',
-      //   data: {
-      //       labels: arrLocalStorageKeys,
-      //       datasets: [{
-      //           data: arrLocalStorageValues,
-      //           backgroundColor: [
-      //               'rgba(255, 99, 132, 0.2)',
-      //               'rgba(54, 162, 235, 0.2)',
-      //               'rgba(255, 206, 86, 0.2)',
-      //               'rgba(75, 192, 192, 0.2)',
-      //               'rgba(153, 102, 255, 0.2)',
-      //               'rgba(255, 159, 64, 0.2)'
-      //           ],
-      //           borderColor: [
-      //               'rgba(255, 99, 132, 1)',
-      //               'rgba(54, 162, 235, 1)',
-      //               'rgba(255, 206, 86, 1)',
-      //               'rgba(75, 192, 192, 1)',
-      //               'rgba(153, 102, 255, 1)',
-      //               'rgba(255, 159, 64, 1)'
-      //           ],
-      //           borderWidth: 1
-      //       }]
-      //   },
-      //   options: {
-      //       scales: {
-      //           y: {
-      //               beginAtZero: true
-      //           }
-      //       }
-      //   }
-      // });
-      //addData(myChart,arrLocalStorageKeys, arrLocalStorageValues);
-
       myChart = new Chart("myChart", chartOptions);
       myChart2 = new Chart("myChart2", chartOptions);
     }
+
     correctAnswers = 0;
     partida++;
   }
@@ -217,6 +173,7 @@ function showQuestion(question) {
     if (answer.correct) {
       button.dataset.correct = true;
     }
+
     button.addEventListener("click", () => {
       selectAnswer()
       if (button.dataset.correct) {
@@ -224,6 +181,7 @@ function showQuestion(question) {
       }
       correcta()
     });
+
     answerButtonsElement.appendChild(button);
   });
 }
@@ -238,16 +196,13 @@ function resetState() {
 function setNextQuestion() {
   resetState();
   showQuestion(questions[currentQuestionIndex]);
-
 }
 
 function startGame() {
   startButton.classList.add("hide");
   textContainer.classList.add("hide");
-
   textCorrect.classList.add("hide");
   textWrong.classList.add("hide");
-  //ctx.classList.add("hide");
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   audio.pause();
@@ -260,36 +215,3 @@ nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
 });
-
-// GRÁFICA
-// const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
-
-//   const data = {
-//     labels: labels,
-//     datasets: [{
-//       label: 'Mi primera gráfica',
-//       backgroundColor: 'rgb(255, 99, 132)',
-//       borderColor: 'rgb(255, 99, 132)',
-//       data: [0, 10, 5, 2, 20, 30, 45],
-//     }]
-//   };
-
-//   const config = {
-//     type: 'bar',
-//     data: data,
-//     options: {}
-//   };
-
-// const myChart = new Chart('myChart', config);
-
-// IMÁGENES RÁNDOM para cada pregunta
-// Dejo esto comentado hasta que vuelva a funcionar la página porque no sé dónde ponerlo
-
-// const imgs = ['./assets/Number_1', './assets/Number_2', './assets/Number_5', './assets/Number_12', './assets/Number_14', './assets/Number_21', './assets/Number_22', './assets/Number_29', './assets/Number_36', './assets/Number_50', './assets/Number_51', './assets/Number_59', './assets/Number_66', './assets/Number_67', './assets/Number_79', './assets/Number_85', './assets/Number_111', './assets/Number_314', './assets/Number_600', './assets/Number_908']
-
-// for (let imagen of imgs){
-//     button.addEventListener('click', changeImg);
-//     function changeImg (){
-//         imagen.setAttribute('src', getRandom(imgs))
-//     }
-// }
